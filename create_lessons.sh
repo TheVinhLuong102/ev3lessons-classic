7e7213a07ea8f45ba742f222e2526b2b536c92dc
Sanjay Seshan <sanjay.seshan@outlook.com>
2017-08-05 21:06:47 -0400

    fixes

--- create_lessons.sh
+++ create_lessons.sh
@@ -31,8 +31,8 @@ for filepath in `ls -1 | awk -F '.' '{print $1}' | fgrep -v index | uniq`; do
   echo "<a href='index.html'><img src='http://archive.ev3lessons.com/web/EV3lessons-EV3Trainer-v1/advanced/back_arrow.jpg'></a>" > $OUTPUT
   echo "<style>h1 {color:red;font-family:verdana;font-size:200%;} b  {    color:green;    font-family:arial;    font-size:100%} p  {    color:brown;    font-family:arial;    font-size:110%}</style>" >> $OUTPUT
   echo "<h1>$path</h1>" >> $OUTPUT
-  echo "  <a href='"$path".pptx'><img src='http://archive.ev3lessons.com/web/EV3lessons-EV3Trainer-v1/advanced/Calibrate/powerpoint.svg'  width='30' height='30'> $path - Microsoft Powerpoint</a><br>" >> $OUTPUT
-  echo "  <a href='"$path".pdf'><img src='http://cwidaho.cc/sites/all/themes/omega_cwi/images/icons/adobe-pdf-icon.png'  width='30' height='30'> $path - Microsoft Powerpoint</a><br>" >> $OUTPUT
+  echo "  <a href='"$path".pptx'><img src='http://icons.iconarchive.com/icons/dakirby309/simply-styled/256/Microsoft-PowerPoint-2010-icon.png'  width='30' height='30'> $path - Microsoft Powerpoint</a><br>" >> $OUTPUT
+  echo "  <a href='"$path".pdf'><img src='http://cwidaho.cc/sites/all/themes/omega_cwi/images/icons/adobe-pdf-icon.png'  width='30' height='30'> $path - Adobe PDF</a><br>" >> $OUTPUT
 #  echo "  <UL>" >> $OUTPUT
 #  for i in `find "$filepath" -maxdepth 1 -mindepth 1 -type f| sort`; do
 #    file=`basename "$i"`
--- create_master.sh
+++ create_master.sh
@@ -7,7 +7,7 @@ OUTPUT="index.html"
 i=0
 echo "<style>h1 {color:red;font-family:verdana;font-size:200%;} b  {    color:green;    font-family:arial;    font-size:100%} p  {    color:brown;    font-family:arial;    font-size:110%}</style>" > $OUTPUT
 echo "<h1>EV3lessons.com v4.9.5 Simple Edition (v1.5.1 Code Base)</h1>" >> $OUTPUT
-echo "<p>This is an auto-generated site with just the English lessons. This contains just the bare minimum js/css/html to download the content. It is intended for use on old browsers/computers or networks with very low bandwith.</p>" >> $OUTPUT
+echo "<p>This is an auto-generated site with just the English lessons. This contains just the bare minimum js/css/html to download the content. It is intended for use on old browsers/computers. </p><b>You have been redirected here because you are using an old browser (older than Microsoft Internet Explorer 9.0)</b><br>" >> $OUTPUT
 #echo "<UL>" > $OUTPUT
 for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d|fgrep -v .git| sort`; do
   path=`basename "$filepath"`
